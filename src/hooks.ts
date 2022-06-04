2f62dc6723dded15f682663fc6a18908cdd31202
Matej Baco <matejbacocom@gmail.com>
2022-06-04 14:30:20 +0200

    Init commit

---
+++ src/hooks.ts
@@ -0,0 +1,11 @@
+import * as cookie from 'cookie';
+
+export function getSession(event: any) {
+    const cookieStr = event.request.headers.get("cookie");
+    const cookies = cookie.parse(cookieStr || '');
+    const authCookie = cookies[`a_session_${import.meta.env.VITE_APPWRITE_PROJECT_ID.toLowerCase()}_legacy`];
+
+    return {
+        authCookie
+    }
+}
